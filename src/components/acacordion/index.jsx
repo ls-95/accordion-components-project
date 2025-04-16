ed866daa5b34e8437d5943231b6c284e5522d27c
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-04-16 14:55:09 +0200

    displayed fake data and added a toggle effect to display the answers

--- src/components/acacordion/index.jsx
+++ src/components/acacordion/index.jsx
@@ -1,3 +1,39 @@
+//single selection
+//multiple selection
+import { useState } from "react";
+import data from "./data";
+
 export default function Accordion() {
-  return <div>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  console.log(selected);
+
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem, index) => (
+            <div className="item" key={index}>
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+                {selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : null}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
 }

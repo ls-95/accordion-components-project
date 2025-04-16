af2ca8ce5f902446b71cbac4592cc6b6f21abd88
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-04-16 09:10:53 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Accordion from "./components/acacordion";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
